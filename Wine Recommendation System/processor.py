274ec074fabd3d7731a2624208f2f7327dd8d883
JISOO JUNG <jisooj@uw.edu>
2018-02-26 23:06:55 -0800

    added punctuation filter table

--- Wine Recommendation System/processor.py
+++ Wine Recommendation System/processor.py
@@ -1,24 +1,39 @@
+"""
+Scans 130k wine reviews and prints out the word frequencies
+used throughout all the reviews
+"""
+
 import json
 from pprint import pprint
 
-# data = json.load(open('data.json'))
 
-# pprint(data)
+def countWords(reviewCount):
+	data = json.load(open('winemag-data-130k-v2.json'))
+	vocabMap = {};
+	punctuations = '-–—.,?!\'":;‘()“”’…`¬\r\n';
+	filterTable = str.maketrans(dict.fromkeys(punctuations))
+	# x = "\'a\'sd\' hi, my name's sam.!??ASdda"
+	# print(x.translate(filterTable))
 
-# pprint(data["maps"])
-# pprint(data["masks"])
+	for i in range(1, reviewCount + 1):
+		printProgress(i, reviewCount)
+		review = data[i]
+		words = review["description"].translate(filterTable).upper().strip().split(" ")
+		for word in words:
+			if len(word) > 0:
+				if word not in vocabMap:
+					vocabMap[word] = 0
+				vocabMap[word] += 1
+
+	pprint(vocabMap)
+	print(len(vocabMap))
+
+def printProgress(i, reviewCount):
+	if (i % int(reviewCount / 10) == 0):
+		percentage = int(i / reviewCount * 100)
+		print(f"{percentage}% : Processed {i} reviews...")
 
 
-data = json.load(open('winemag-data-130k-v2.json'))
-vocabMap = {};
-for i in range(1,100000):
-	review = data[i]
-	description = review["description"].replace(",", "").replace(".", "").upper().strip().split(" ")
-	for word in description:
-		if word not in vocabMap:
-			vocabMap[word] = 0
-		vocabMap[word] += 1
 
-pprint(vocabMap)
-print(len(vocabMap))
-
+reviewCount = 50000
+countWords(reviewCount)
